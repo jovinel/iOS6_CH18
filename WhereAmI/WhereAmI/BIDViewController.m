5dbf6abed5f8f4a70740860649dd4d2ddb8f5688
crescens <crescens.techstars@gmail.com>
2013-12-10 14:02:59 +0800

    Finished a simple demo for getting the latitude, longitude, h-accuracy, altitude, v-accuracy, and distance tavelled.

--- WhereAmI/WhereAmI/BIDViewController.m
+++ WhereAmI/WhereAmI/BIDViewController.m
@@ -18,12 +18,57 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [_locationManager startUpdatingLocation];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark -
+#pragma mark CLLocationManagerDelegate Methods
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *newLocation = [locations lastObject];
+    NSString *latitudeString = [NSString stringWithFormat:@"%g\u00B0", newLocation.coordinate.latitude];
+    _latitudeLabel.text = latitudeString;
+    
+    NSString *longitudeString = [NSString stringWithFormat:@"%g\u00B0", newLocation.coordinate.longitude];
+    _longitudeLabel.text = longitudeString;
+    
+    NSString *horizontalAccuracyString = [NSString stringWithFormat:@"%gm", newLocation.horizontalAccuracy];
+    _horizontalAccuracyLabel.text = horizontalAccuracyString;
+    
+    NSString *altitudeString = [NSString stringWithFormat:@"%gm", newLocation.altitude];
+    _altitudeLabel.text = altitudeString;
+    
+    NSString *verticalAccuracyString = [NSString stringWithFormat:@"%gm", newLocation.verticalAccuracy];
+    _verticalAccuracyLabel.text = verticalAccuracyString;
+    
+    if (newLocation.verticalAccuracy < 0 || newLocation.horizontalAccuracy < 0) {
+        // invalid accuracy
+        return;
+    }
+    if (newLocation.horizontalAccuracy > 100 || newLocation.verticalAccuracy > 50) {
+        // accuracy radius is so large, we don't want to use it
+        return;
+    }
+    if (_startPoint == nil) {
+        self.startPoint = newLocation;
+        self.distanceFromStart = 0;
+    } else {
+        self.distanceFromStart = [newLocation distanceFromLocation:_startPoint];
+    }
+    
+    NSString *distanceString = [NSString stringWithFormat:@"%g", _distanceFromStart];
+    _distanceTraveledLabel.text = distanceString;
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSString *errorType = (error.code == kCLErrorDenied) ? @"Access Denied" : @"Unknown Error";
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error getting location" message:errorType delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+    [alert show];
+
 }
 
 @end
